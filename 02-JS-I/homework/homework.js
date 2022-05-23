cfbb9560a230a2ed259af61c764b0f52c59029ae
Luis Condori <luis2003nb@gmail.com>
2022-05-23 16:36:02 -0300

    hahahaha

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -69,7 +69,10 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  return ("1cm","1cm,");
+  if (str1.length === str2.length){
+    return true 
+  } 
+  return false
 }
 
 function menosQueNoventa(num) {
@@ -148,21 +151,29 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  return numero + " Es positivo";
+  if (numero > 0){ 
+  return "Es positivo"
+}
+  else if (numero < 0 ){ 
+  return "Es negativo"
+}
+  else if (numero == 0){
+  return false
+  }
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
-  return  str;
+  return  str + "!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  return ("Pablo" , "Martinez");
+  return (nombre + " " + apellido);
 }
 
 function obtenerSaludo(nombre) {
@@ -170,8 +181,9 @@ function obtenerSaludo(nombre) {
   // "Martin" -> "Hola Martin!"
   // Tu código:
 
-  return hola + nombre;
+  return "Hola " + nombre + "!" ;
 }
+
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
@@ -197,7 +209,10 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  return  (euro == dolar) == 1.20;
+  
+  var equivalencia = euro * 1.20; 
+  
+  return equivalencia;
 
 }
 
@@ -208,7 +223,15 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+if (letra.length  > 1)
+{
+  return "Dato incorrecto"
+}
+if (letra == "a" || letra == "e" || letra == "i" || letra == "o"|| letra == "u"){
+  return "Es vocal";
+}else { 
+  return "Dato incorrecto";
+}
 }
 
 
