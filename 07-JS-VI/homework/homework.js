ddf18108b0a0c4a64a0bdf1ffd802248418919e5
Luis Condori <luis2003nb@gmail.com>
2022-07-10 23:08:12 -0300

    hahaha

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,6 +4,12 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  nombre.toLowerCase()
+ let letramayuscula=nombre[0].toUpperCase();
+ let letrasres = nombre.slice(1)
+ 
+ 
+ return letramayuscula + letrasres;
 }
 
 function invocarCallback(cb) {
